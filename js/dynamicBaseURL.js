26056ac0b94b35cc7e97fd2769df0426a979a241
SunnyDineva <sunny0912@gmail.com>
2025-02-18 15:17:12 +0200

    dynamicBaseURL

--- js/dynamicBaseURL.js
+++ js/dynamicBaseURL.js
@@ -1,5 +1,5 @@
 if(window.location.hostname === "sunnydineva.github.io") {
-  document.write('base href="https://sunnydineva.github.io/">')
+  document.write('base href="https://sunnydineva.github.io/magic-numbers">')
 } else {
   document.write('base href="/">')
 }
